de7c08a15d6ba59b5c71b84f8e614d488f8b0213
Princi Rakotoarison <princirakotoarison07@gmail.com>
2026-02-10 17:39:08 +0300

    creation api pour le liste reservation et hotels, REST

--- src/main/java/com/taxi/controller/ReservationController.java
+++ src/main/java/com/taxi/controller/ReservationController.java
@@ -7,12 +7,21 @@ import framework.annotation.GetMapping;
 import framework.annotation.PostMapping;
 import framework.annotation.ModelAttribute;
 import framework.utilitaire.ModelAndView;
+import framework.annotation.RestController;
 import java.sql.Connection;
-import java.sql.DriverManager;
+import java.util.List;
 import java.sql.Timestamp;
 
 @Controller
+@RestController
 public class ReservationController {
+
+    @GetMapping("/api/reservations")
+    public List<Reservation> listReservations() throws Exception {
+        try (Connection conn = DBConnection.getConnection()) {
+            return Reservation.getAll(Reservation.class, conn);
+        }
+    }
 
     @GetMapping("/reservation/form")
     public ModelAndView showForm() {
