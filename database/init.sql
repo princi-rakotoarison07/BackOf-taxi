f14d5ae63ee52b43ef955d5b4eb4bf68a7753c08
Tendriniavvo <tendryniavo76@gmail.com>
2026-03-11 18:49:29 +0300

    del data

--- database/init.sql
+++ database/init.sql
@@ -105,19 +105,7 @@ INSERT INTO distance (id_distance, lieu_from, lieu_to, kilometre) VALUES
 ('DIST004', 'LIEU001', 'LIEU008', 20.10), -- Aéroport -> Hotel Lokanga (Lieu 008)
 
 -- Distances Inter-hôtels (Exemples)
-('DIST005', 'LIEU005', 'LIEU010', 4.50),
-('DIST015', 'LIEU010', 'LIEU005', 10),
-('DIST006', 'LIEU005', 'LIEU006', 2.30),
-('DIST007', 'LIEU005', 'LIEU008', 3.10),
-('DIST008', 'LIEU010', 'LIEU006', 5.20),
-('DIST009', 'LIEU010', 'LIEU008', 6.40),
-('DIST010', 'LIEU006', 'LIEU008', 2.80),
-
--- Retours Aéroport (Mêmes distances que l'aller pour la simplicité)
-('DIST011', 'LIEU005', 'LIEU001', 18.50),
-('DIST012', 'LIEU010', 'LIEU001', 15.20),
-('DIST013', 'LIEU006', 'LIEU001', 17.80),
-('DIST014', 'LIEU008', 'LIEU001', 20.10);
+('DIST005', 'LIEU010', 'LIEU005', 4.50);
 
 INSERT INTO vehicule (id_vehicule, reference, nbr_place, id_type_carburant) VALUES
 ('VH001', 'Toyota Hiace D1', 12, 'TC0001'),
